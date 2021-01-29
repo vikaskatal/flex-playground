dc15364f9df32db0be4d761b5aa7a8d8b30ad45d
vikaskatal <katal.viku@gmail.com>
2021-01-29 18:37:54 +0530

    Add: slider+addItem+coloredItems; Update: layout;

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let flexItemCount = 2;
+
 const flexObj = {
   // Init Function
   init: function(){
@@ -35,19 +37,21 @@ const flexObj = {
   // Build Flex Container Html Element
   buildFlexContainer: function(){
     let html = '';
-    html += `<div class="flex-container" id="parentContainer" style="">`;
+    html += `<div class="playground-container" id="parentContainer">`;
     html += `</div>`;
-    document.getElementById("flexRoot").innerHTML = html;
+    document.getElementById("flexPlayground").innerHTML = html;
   },
 
   // Build Flex Child Html Elements
   buildFlexChildren: function(){
     let inputChildNumbers = document.getElementById('inputChildNumbers');
-    let inputValue = inputChildNumbers.value;
+    let inputValue = 2;//inputChildNumbers.value;
     let html = '';
+    
     if(inputValue && inputValue > 0){
       for(let i = 0; i < inputValue; i++){
-        html += `<div style=""> ${i+1}`;
+        let randomColor = colors[Math.floor(Math.random() * colors.length)]
+        html += `<div class="playground-items" data-item="${i+1}" onclick="flexObj.removeFlexChild(${i+1})" style="background-color: ${randomColor}"> ${i+1}`;
         html += `</div>`;
       }
     }else{
@@ -56,21 +60,45 @@ const flexObj = {
     }
     document.getElementById("parentContainer").innerHTML = html;
   },
+  createFlexChild: function () {
+    
+  },
+  addFlexChild: function(){
+    let node = document.createElement("DIV");
+    node.classList.add('playground-items');
+
+    // let count = document.querySelectorAll('.playground-items').length;
+    // Update global variable onclick as well as removeFlexChild 
+    flexItemCount = flexItemCount+1;
+
+    let textnode = document.createTextNode(`${flexItemCount}`);
+    node.appendChild(textnode);
+
+    let randomColor = colors[Math.floor(Math.random() * colors.length)]
+    node.style.backgroundColor = randomColor;
+
+    node.setAttribute("data-item", flexItemCount);
+    node.setAttribute("onclick", `flexObj.removeFlexChild(${flexItemCount})`);
+
+    document.getElementById("parentContainer").appendChild(node);
+  },
+  
+  removeFlexChild: function(index){
+    if(index){
+      let myobj = document.querySelectorAll(`[data-item="${index}"]`);
+      if(myobj){
+        myobj[0].remove(); 
+      }
+    }
+  },
 
   //Change Children Count from Input 
   changeChildrenCount: function(value){
-    // console.log(value)
     this.init();
   },
 
   //Change Styles of Flex Html Elements
   changeStyles: function(key, value){
-
-    // console.log(key, value)
-    // if(key === 'alignContent'){
-    //   parentContainer.style.height = 600;
-    // }
-
     if(key && value){
       parentContainer.style[key] = value;
     }
@@ -78,3 +106,37 @@ const flexObj = {
 }
 
 flexObj.init();
+// Construct Flex playground and Properties list
+
+
+const slider = {
+  inputEle: document.getElementById("myRange"),
+  flexItems: document.querySelectorAll('.playground-items'),
+  minValue: 0,
+  maxValue: 500,
+  currentValue: 80,
+  changeFlexItemsStyles: function(key, value){
+    let items = document.querySelectorAll('.playground-items');
+    if(key && value){
+      for (let i = 0; i < items.length; i++) {
+        items[i].style[key] = value;
+      }
+    }
+  },
+  init: function(){
+    console.log('d', this.inputEle)
+    this.inputEle.setAttribute("min", this.minValue);
+    this.inputEle.setAttribute("max", this.maxValue);
+    this.inputEle.setAttribute("value", this.currentValue);
+    this.changeFlexItemsStyles('minWidth', this.currentValue);
+  }
+}
+
+slider.init();
+ 
+slider.inputEle.oninput = function() {
+  let widthValue = this.value;
+  slider.changeFlexItemsStyles('minWidth', widthValue);
+}
+
+// Range Slider
